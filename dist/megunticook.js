6061ab7c1ab877b65bd1d4e2c40495515cdcbefe
Charles O Strout <charlie@sevenshadow.com>
2016-02-22 00:07:44 -0500

    Changed to Module

--- dist/megunticook.js
+++ dist/megunticook.js
@@ -1,25 +1,23 @@
-define('megunticook',[] , function () {
+var megunticook = function () {
+    'use strict';
         
-        var simpleReArgs = {
-            requireReturn: 0,
-            bookValuePerShareAtTime0 : 0,
-            valuePerShareAtTime0 : 0,
-            earningsPerShareAtTime1 : 0,
-            earningsPerShareAtTime2 : 0,
-            dividendAtTime1 : 0
-        };
-        
-        var getSimpleReValue = function(simpleReArgs)
-        {
-            
-            return simpleReArgs.valuePerShareAtTime0;
-        };
-        
-        var publicApi = {
-            simpleReArgs : simpleReArgs,
-            getSimpleReValue : getSimpleReValue
-        };
-        
-        return publicApi;
+    var simpleReArgs = {
+        requireReturn: 0,
+        bookValuePerShareAtTime0 : 0,
+        valuePerShareAtTime0 : 0,
+        earningsPerShareAtTime1 : 0,
+        earningsPerShareAtTime2 : 0,
+        dividendAtTime1 : 0
+    };
     
-});
+    var getSimpleReValue = function(simpleReArgs)
+    {
+        
+        return simpleReArgs.valuePerShareAtTime0;
+    };
+    
+    return {
+        simpleReArgs : simpleReArgs,
+        getSimpleReValue : getSimpleReValue
+    };
+};
